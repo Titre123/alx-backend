bd9115b8950b800968fe530201a7e0e67a05bdb0
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2023-01-24 02:58:32 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,7 +6,12 @@ import math
 from typing import List
 
 
-index_range = __import__('0-simple_helper_function').index_range
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return tuple([start_index, end_index])
 
 
 class Server:
