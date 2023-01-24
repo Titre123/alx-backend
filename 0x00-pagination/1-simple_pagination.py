25790cbe6ad8338462b14a3822cf40b74ee7c910
Taiwo <taiwotriumphant@gmail.com>
2023-01-24 16:10:36 +0100

    implemented the dataset update

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,7 +38,7 @@ class Server:
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         range = index_range(page, page_size)
-        data = self.dataset()
-        if range[0] > len(data):
+        self.dataset()
+        if range[0] > len(self.__dataset):
             return []
-        return data[range[0]:range[1]]
+        return self.__dataset[range[0]:range[1]]
