5ec6a22684ba7fea49a9aa677e209deb802c7581
Taiwo <taiwotriumphant@gmail.com>
2023-01-24 04:03:26 +0100

    2-hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+"""Simple pagination sample.
+"""
 import csv
-import math
-from typing import List, Dict
+from typing import List, Tuple
 
 
-index_range = __import__('0-simple_helper_function').index_range
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return tuple([start_index, end_index])
 
 
 class Server:
@@ -26,19 +33,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''
-            Args:
-                - page: int
-                - page_size: int
-            return
-                - List
-        '''
-        assert type(page) == int
-        assert type(page_size) == int
-        assert page > 0
-        assert page_size > 0
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
         range = index_range(page, page_size)
-        return self.__dataset[range[0]:range[1]]
+        data = self.dataset()
+        if range[0] > len(data):
+            return []
+        return data[range[0]:range[1]]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         '''
@@ -48,4 +51,23 @@ class Server:
             return
                 - List
         '''
+        total_pages = len(self.__dataset) // page_size
+        if (len(self.__dataset) % page_size != 0):
+            total_pages += 1
+        prev_page = None
+        next_page = None
+        if page > 1:
+            prev_page = page - 1
+        if page < total_pages:
+            next_page = page + 1
+        data = self.get_page(page, page_size)
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
+        
         
