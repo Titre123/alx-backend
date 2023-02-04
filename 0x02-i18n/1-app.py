558cd442e97a4f4b7db16fa201f98add6e410399
Taiwo <taiwotriumphant@gmail.com>
2023-02-04 03:46:46 +0100

    code formatting

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,25 +9,23 @@ import pytz
 
 
 class Config:
-    '''Config class for configuration'''
-    LANGUAGES = ['en', 'fr']
-
-    DEFAULT_LOCALE = 'en'
-
-    TIMEZONE = pytz.utc
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = pytz.utc
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
-# babel
 babel = Babel(app)
 
 
 @app.route('/')
 def Home():
     '''Home route'''
-    return render_template('./0-index.html')
+    return render_template('./1-index.html')
 
 
 if __name__ == "__main__":
