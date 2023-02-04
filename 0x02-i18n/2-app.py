b78410fa2f3974e21a320ae884e99a9b86722834
Taiwo <taiwotriumphant@gmail.com>
2023-02-04 03:52:20 +0100

    code formatting

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,25 +1,35 @@
+#!/usr/bin/env python3
+'''
+    Flask application
+'''
+
 from flask import Flask, render_template, request
-# ...
 from flask_babel import Babel
+import pytz
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-
-Config = __import__('config').Config
-
-app.config['DEFAULT_LOCALE'] = Config.DEFAULT_LOCALE
-app.config['TIMEZONE'] = Config.TIMEZONE
-
-app.config['LANGUAGES'] = Config.LANGUAGES
-
-#babel
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
+
 
 @app.route('/')
 def Home():
+    '''Home route'''
     return render_template('./2-index.html')
 
 @babel.localeselector
 def get_locale():
+    '''method to set language'''
     return request.accept_languages.best_match(['en', 'fr'])
 
 
